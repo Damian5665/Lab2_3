83e7bd0f92a39d6de38d66cb420fac961816b2c8
Damian56651 <126695965+Damian5665@users.noreply.github.com>
2023-03-15 15:19:26 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int i,s=0;
+    for(i=0;i<101;i++)
+      {
+        if(i%2==0)
+        {
+          s=s+i;
+        }
+      }
+      System.out.println(s);
+  }
+}
